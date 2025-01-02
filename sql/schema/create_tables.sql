9d9afa5a0e4c368bec342456ba0e0d9c97fa5621
Gerrid Rose <gerrid.rose@gmail.com>
2025-01-01 19:48:07 -0700

    Very close now to calling this a good starting draft schema.

--- sql/schema/create_tables.sql
+++ sql/schema/create_tables.sql
@@ -1,20 +1,20 @@
 -- Switch to the twisted_spur schema
 SET search_path TO twisted_spur;
 
--- Create a table named 'orders' within the custom_shirts_store schema
+-- Create a table named 'users' within the twisted_spur schema
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    first_name TEXT,
-    last_name TEXT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
     -- not sure what to do with this if passwd is removed
-    email TEXT,
+    email TEXT NOT NULL,
     -- eventually passwd should be removed, and another authentication
     -- provider should be used externally
-    passwd TEXT,
-    birthday DATE,
-    phone_number TEXT,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    passwd TEXT NOT NULL, -- DO NOT store clear text of this
+    birthday DATE NOT NULL,
+    phone_number TEXT NOT NULL,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE addresses (
@@ -28,8 +28,8 @@ CREATE TABLE addresses (
     city TEXT,
     postal_code TEXT,
     phone_number TEXT,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
 ALTER TABLE addresses
@@ -40,109 +40,108 @@ REFERENCES users (id);
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     category TEXT,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
-CREATE TYPE transfer_types AS ENUM (
-    'DTF',
-    'SUBLIMATION',
-    'GANG_SHEET',
-    'SCREEN_PRINT'
+CREATE TABLE transfer_types (
+    id SERIAL PRIMARY KEY,
+    transfer_type TEXT NOT NULL,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
-CREATE TABLE transfers (
+CREATE TABLE prints (
     id SERIAL PRIMARY KEY,
     category_id INTEGER,
-    transfer_type transfer_types,
+    transfer_type_id INTEGER,
     -- should a larger range be used here?
-    price NUMERIC(12,2),
-    url_to_image TEXT,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    price NUMERIC(12,2) NOT NULL,
+    url_to_print TEXT NOT NULL,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
-ALTER TABLE transfers
+ALTER TABLE prints
 ADD CONSTRAINT fk_category_id
 FOREIGN KEY (category_id) 
 REFERENCES categories (id);
 
-CREATE TYPE shirt_type AS ENUM (
-    'T_SHIRT',
-    'LONG_SLEEVE',
-    'CREW_NECK',
-    'HOODIE',
-    'T_SHIRT_KIDS',
-    'CREW_NECK_KIDS'
-);
-
-CREATE TYPE shirt_size AS ENUM (
-    'SMALL',
-    'MEDIUM',
-    'LARGE',
-    'XL',
-    'XXL',
-    'XXXL',
-    '4XL',
-    '5XL'
-);
-
-CREATE TABLE shirt_colors (
-    id SERIAL PRIMARY KEY,
-    color_name TEXT,
-    hex_rep BYTEA,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
-);
+ALTER TABLE prints
+ADD CONSTRAINT fk_transfer_type_id
+FOREIGN KEY (transfer_type_id) 
+REFERENCES transfer_types (id);
 
 CREATE TABLE suppliers (
     id SERIAL PRIMARY KEY,
     brand TEXT,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
-CREATE TABLE shirt_products (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     category_id INTEGER,
-    supplier_id INTEGER,
-    shirt_style shirt_type,
-    shirt_size shirt_size,
-    shirt_color_id INTEGER,
-    transfer_id INTEGER,
     price numeric(12,2),
     cost numeric(12,2),
     quantity INTEGER,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
-ALTER TABLE shirt_products
+ALTER TABLE products
 ADD CONSTRAINT fk_category_id
 FOREIGN KEY (category_id) 
 REFERENCES categories (id);
 
-ALTER TABLE shirt_products
+CREATE TABLE product_skus (
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER NOT NULL,
+    supplier_id INTEGER NOT NULL,
+    print_id INTEGER NOT NULL,
+    sku TEXT,
+    price numeric(12,2),
+    cost numeric(12,2),
+    quantity INTEGER,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+ALTER TABLE product_skus
+ADD CONSTRAINT fk_product_id
+FOREIGN KEY (product_id) 
+REFERENCES products (id);
+
+ALTER TABLE product_skus
 ADD CONSTRAINT fk_supplier_id
 FOREIGN KEY (supplier_id) 
 REFERENCES suppliers (id);
 
-ALTER TABLE shirt_products
-ADD CONSTRAINT fk_shirt_color_id
-FOREIGN KEY (shirt_color_id) 
-REFERENCES shirt_colors (id);
+ALTER TABLE product_skus
+ADD CONSTRAINT fk_print_id
+FOREIGN KEY (print_id) 
+REFERENCES prints (id);
 
-ALTER TABLE shirt_products
-ADD CONSTRAINT fk_transfer_id
-FOREIGN KEY (transfer_id) 
-REFERENCES transfers (id);
+CREATE TABLE sku_attributes (
+    id SERIAL PRIMARY KEY,
+    product_sku_id INTEGER,
+    attribute_type TEXT NOT NULL,
+    attribute_value TEXT NOT NULL,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+ALTER TABLE sku_attributes
+ADD CONSTRAINT fk_product_sku_id
+FOREIGN KEY (product_sku_id) 
+REFERENCES categories (id);
 
 CREATE TABLE cart (
     id SERIAL PRIMARY KEY,
     user_id INTEGER,
     total NUMERIC(12,2),
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
 ALTER TABLE cart
@@ -153,10 +152,10 @@ REFERENCES users (id);
 CREATE TABLE cart_item(
     id SERIAL PRIMARY KEY,
     cart_id INTEGER,
-    product_id INTEGER,
+    product_sku_id INTEGER,
     quantity INTEGER,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
 );
 
 ALTER TABLE cart_item
@@ -165,25 +164,91 @@ FOREIGN KEY (cart_id)
 REFERENCES cart (id);
 
 ALTER TABLE cart_item
-ADD CONSTRAINT fk_product_id
-FOREIGN KEY (product_id) 
-REFERENCES products (id);
+ADD CONSTRAINT fk_product_sku_id
+FOREIGN KEY (product_sku_id) 
+REFERENCES product_skus (id);
 
---     order_number VARCHAR(50),
---     number_of_shirts INTEGER,
---     status VARCHAR(50),
---     transfer_status VARCHAR(50),
---     shirt_status VARCHAR(50),
---     order_date DATE,
---     cost NUMERIC(10, 2),
---     price NUMERIC(10, 2),
---     tax NUMERIC(10, 2),
---     price_with_tax NUMERIC(10, 2),
---     profit NUMERIC(10, 2),
---     payment_system VARCHAR(100),
---     payment_status VARCHAR(50),
---     sales_platform VARCHAR(100),
---     notes TEXT
+CREATE TABLE order_statuses (
+    id SERIAL PRIMARY KEY,
+    order_status TEXT,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE order_details (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    payment_id INTEGER,
+    total NUMERIC(12,2),
+    order_status INTEGER,
+    order_notes TEXT,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+ALTER TABLE order_details
+ADD CONSTRAINT fk_user_id
+FOREIGN KEY (user_id)
+REFERENCES users (id);
+
+CREATE TABLE payment_systems (
+    id SERIAL PRIMARY KEY,
+    payment_system TEXT,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE payment_states (
+    id SERIAL PRIMARY KEY,
+    payment_state TEXT,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE payment_details (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER,
+    amount NUMERIC(12,2),
+    payment_system INTEGER,
+    payment_status INTEGER,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+ALTER TABLE payment_details
+ADD CONSTRAINT fk_order_id
+FOREIGN KEY (order_id)
+REFERENCES order_details (id);
+
+ALTER TABLE payment_details
+ADD CONSTRAINT fk_payment_system
+FOREIGN KEY (payment_system)
+REFERENCES payment_systems (id);
+
+ALTER TABLE payment_details
+ADD CONSTRAINT fk_payment_status
+FOREIGN KEY (payment_status)
+REFERENCES payment_states (id);
+
+CREATE TABLE order_item (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER,
+    product_id INTEGER,
+    quantity INTEGER,
+    order_item_notes TEXT,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP
+);
+
+ALTER TABLE order_item
+ADD CONSTRAINT fk_order_id
+FOREIGN KEY (order_id)
+REFERENCES order_details (id);
+
+ALTER TABLE order_details
+ADD CONSTRAINT fk_payment_id
+FOREIGN KEY (payment_id)
+REFERENCES payment_details (id);
 
 -- -- List all schemas in the database
 -- SELECT schema_name
